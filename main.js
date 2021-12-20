084c3d8c88b870b5453c3e8763cd090b748e7ad0
ABDURAHMON123 <nazirjonovabdurahmon123@gmail.com>
2021-12-20 16:29:16 +0500

    KOIN sayt polniy bitdi Alhamdulillah

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ document.querySelector('.fa-close').addEventListener('click', function() {
     let mobilemenu = document.querySelector('.header__mobile__menu');
     mobilemenu.style.transform = 'translateY(-200%)'
 })
+document.querySelector('.theproduct__blocks').addEventListener('scroll', function() {
+    let theproduct = document.querySelector('.theproduct__blocks'); -
+    console.log('scrolllll');
+})
